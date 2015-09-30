258f41889ca4df615005573357b68a921b637558
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2015-09-30 03:52:25 -0700

    removed useless assignments to string

--- util/mtwilson-util-tpm12/src/main/java/gov/niarl/his/privacyca/TpmKeyParams.java
+++ util/mtwilson-util-tpm12/src/main/java/gov/niarl/his/privacyca/TpmKeyParams.java
@@ -100,7 +100,7 @@ public class TpmKeyParams {
 	 * @return
 	 */
 	public static String esToString(short es, boolean TrouSerSmode) {
-		String returnVal = "";
+		String returnVal;
 		switch (es) {
 		case TPM_ES_NONE:
 			 returnVal = "TPM_ES_NONE";
@@ -134,7 +134,7 @@ public class TpmKeyParams {
 	 * @return
 	 */
 	public static String ssToString(short ss) {
-		String returnVal = "";
+		String returnVal;
 		switch (ss) {
 		case TPM_SS_NONE:
 			returnVal = "TPM_SS_NONE";
