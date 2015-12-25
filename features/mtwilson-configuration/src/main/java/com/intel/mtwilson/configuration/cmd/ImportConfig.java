5edbc274d46f263c456e748232115c6c726f9210
Ryan Savino <ryan.k.savino@intel.com>
2015-12-25 10:21:21 -0800

    dev-cit-3.0-sprint3: added input and output file options to both ExportConfig and ImportConfig while maintaining default functionality

--- features/mtwilson-configuration/src/main/java/com/intel/mtwilson/configuration/cmd/ImportConfig.java
+++ features/mtwilson-configuration/src/main/java/com/intel/mtwilson/configuration/cmd/ImportConfig.java
@@ -9,20 +9,22 @@ import com.intel.dcsg.cpg.crypto.file.PasswordEncryptedFile;
 import com.intel.dcsg.cpg.crypto.key.password.PasswordProtection;
 import com.intel.dcsg.cpg.crypto.key.password.PasswordProtectionBuilder;
 import com.intel.dcsg.cpg.io.FileResource;
+import com.intel.dcsg.cpg.io.pem.Pem;
 import com.intel.mtwilson.Environment;
 import com.intel.mtwilson.MyConfiguration;
 import java.io.File;
 import java.io.FileInputStream;
 import org.apache.commons.io.IOUtils;
 /**
- * 
+ *
  * @author jbuhacoff
  */
 public class ImportConfig extends InteractiveCommand {
+    private final String USAGE = "Usage: import-config <infile|--in=infile|--stdin> [--env-password=PASSWORD_VAR]";
 
     @Override
     public void execute(String[] args) throws Exception {
-        
+
         String password;
         if( options != null && options.containsKey("env-password") ) {
             password = getNewPassword("the Server Configuration File", "env-password");
@@ -31,40 +33,45 @@ public class ImportConfig extends InteractiveCommand {
             // gets environment variable MTWILSON_PASSWORD, TRUSTAGENT_PASSWORD, KMS_PASSWORD, etc.
             password = Environment.get("PASSWORD");
             if( password == null ) {
-                throw new IllegalArgumentException("Usage: ImportConfig <infile|--in=infile|--stdin> [--env-password=PASSWORD_VAR]");
+                throw new IllegalArgumentException(USAGE);
             }
         }
-                
+
+        File outputFile;
+        if( options != null && options.containsKey("out") ) {
+            outputFile = new File(options.getString("out"));
+        } else {
+            MyConfiguration config = new MyConfiguration();
+            outputFile = config.getConfigurationFile();
+        }
+        FileResource out = new FileResource(outputFile);
+
         String content;
         if( options != null && options.containsKey("in") ) {
             try (FileInputStream in = new FileInputStream(new File(options.getString("in")))) {
                 content = IOUtils.toString(in);
             }
-        }
-        else if( options != null && options.getBoolean("stdin", false) ) {
+        } else if( options != null && options.getBoolean("stdin", false) ) {
             content = IOUtils.toString(System.in);
-        }else if( args.length == 1 ) {
+        } else if( args.length == 1 ) {
             String filename = args[0];
             try (FileInputStream in = new FileInputStream(new File(filename))) {
                 content = IOUtils.toString(in);
             }
-        }
-        else {
-            throw new IllegalArgumentException("Usage: ImportConfig <infile|--in=infile|--stdin> [--env-password=PASSWORD_VAR]");
+        } else {
+            throw new IllegalArgumentException(USAGE);
         }
         
+        if (Pem.isPem(content)) {
+            throw new IllegalArgumentException(String.format("File contents are already encrypted\n%s", USAGE));
+        }
+
         PasswordProtection protection = PasswordProtectionBuilder.factory().aes(256).block().sha256().pbkdf2WithHmacSha1().saltBytes(8).iterations(1000).build();
         if( !protection.isAvailable() ) {
-//                log.warn("Protection algorithm {} key length {} mode {} padding {} not available", protection.getAlgorithm(), protection.getKeyLengthBits(), protection.getMode(), protection.getPadding());
+//            log.warn("Protection algorithm {} key length {} mode {} padding {} not available", protection.getAlgorithm(), protection.getKeyLengthBits(), protection.getMode(), protection.getPadding());
             protection = PasswordProtectionBuilder.factory().aes(128).block().sha256().pbkdf2WithHmacSha1().saltBytes(8).iterations(1000).build();
         }
-        
-        MyConfiguration config = new MyConfiguration();
-        FileResource resource = new FileResource(config.getConfigurationFile());
-        PasswordEncryptedFile encryptedFile = new PasswordEncryptedFile(resource, password, protection);
-
+        PasswordEncryptedFile encryptedFile = new PasswordEncryptedFile(out, password, protection);
         encryptedFile.saveString(content);
     }
-    
-
 }
