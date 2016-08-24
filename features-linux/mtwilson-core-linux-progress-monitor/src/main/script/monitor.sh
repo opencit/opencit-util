edbb39b018bbd4d23d53952fabb02a4854b22412
Jonathan Buhacoff <jonathan.buhacoff@intel.com>
2016-08-24 00:03:37 -0700

    improved ability to observe progress in detached mode

--- features-linux/mtwilson-core-linux-progress-monitor/src/main/script/monitor.sh
+++ features-linux/mtwilson-core-linux-progress-monitor/src/main/script/monitor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# parameter:
+# USAGE 1: start a process and monitor its stdout, showing progress bar
+# monitor.sh <target> [marker_file] [working_directory]
+# Parameters:
 # 1. path to (executable) target (e.g. ./mtwilson-server.bin)
 #    REQUIRED.
 # 2. path to progress marker file (e.g. ./mtwilson-server.bin.mark)
@@ -15,6 +17,21 @@
 
 # The target can also be any executable on the path
 
+# USAGE 2: check the status of a monitored process
+# monitor.sh --status <working_directory>
+# Parameters:
+# 1. working directory , for example /tmp/cit/monitor/target.bin
+
+# USAGE 3: monitor a process that is already executing
+#          (do not start it, just look for the status updates and display progress bar)
+# monitor.sh --noexec <working_directory>           if not currently running, the monitor will exit with a message
+# monitor.sh --noexec --wait <working_directory>    if not currently running, the monitor will wait for it indefinitely
+
+# USAGE 2: get the pid of a monitored process
+# monitor.sh --pid <working_directory>
+# Parameters:
+# 1. working directory , for example /tmp/cit/monitor/target.bin
+
 # A progress marker file contains an ORDERED LIST of UNIQUE lines to
 # match in the output of the target. If a line is not unique, only
 # the first occurrence will be used as the marker.
@@ -25,15 +42,6 @@
 # is found, the progress is updated and the script only looks for
 # subquent markers in the output. 
 
-target=$1
-markerfile=$2
-workdir=$3
-
-if [ "$1" == "--help" ]; then
-  echo "Usage: monitor.sh target.bin [target.bin.mark] [/tmp/cit/monitor/target.bin]"
-  exit 1
-fi
-
 # ensure we have paths to standard binaries
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
@@ -42,54 +50,172 @@ TERM_COLOR_GREEN="\\033[1;32m"
 TERM_COLOR_RED="\\033[1;31m"
 TERM_COLOR_NORMAL="\\033[0;39m"
 
-if [ -f "$target" ] && [ -z "$markerfile" ]; then
-  markerfile=${target}.mark
-elif [ ! -f $target ] && [ -z "$markerfile" ]; then
-  is_target_on_path=$(which $target 2>/dev/null)
-  if [ -n "$is_target_on_path" ]; then
-    echo "Target '$target' is on path but marker file not specified"
-    exit 1
+parse_args() {
+if [ "$1" == "--help" ]; then
+  echo "Usage: monitor.sh target.bin [target.bin.mark] [/tmp/cit/monitor/target.bin]"
+  exit 1
+elif [ "$1" == "--status" ]; then
+  shift
+  if [ -n "$1" ] && [ -d "$1" ] && [ -f "$1/status" ]; then
+    workdir=$1
+    cat $workdir/status
+    exit 0
   fi
+  exit 1
+elif [ "$1" == "--pid" ]; then
+  shift
+  if [ -n "$1" ] && [ -d "$1" ] && [ -f "$1/pid" ]; then
+    workdir=$1
+    target_pid=$(cat $workdir/pid 2>/dev/null)
+    if is_target_running $target_pid; then echo $target_pid; fi
+    exit 0
+  fi
+  exit 1
+elif [ "$1" == "--noexec" ]; then
+  MONITOR_NOEXEC=yes
+  shift
+  if [ "$1" == "--wait" ]; then
+    MONITOR_NOEXEC_WAIT=yes
+    shift
+  fi
+  if [ -n "$1" ] && [ -d "$1" ] && [ -f "$1/status" ]; then
+    workdir=$1
+    target=$(basename $1)
+    markerfile=
+
+    if is_target_active_running; then
+      monitor_target
+      exit $?
+    elif [ "$MONITOR_NOEXEC_WAIT" ]; then
+      monitor_target
+      exit $?
+    fi
+  fi
+  echo "target is not currently running"
+  exit 1
+else
+  target=$1
+  markerfile=$2
+  workdir=$3
+  # regular operation
+  create_workdir
+  init_markers
+  execute_target
+  monitor_target
+  exit $?
 fi
-if [ -z "$workdir" ]; then
-  filename=$(basename $target)
-  workdir=/tmp/cit/monitor/$filename
-fi 
-mkdir -p $workdir/markers
-echo "PENDING" > $workdir/status
+}
 
-# split the marker file into individual numbered lines in separate files
-# the grep removes empty lines
-max=$(cat $markerfile | grep -v -e '^$' | wc -l)
-for i in `seq 1 $max`
-do
-  cat $markerfile | grep -v -e '^$' | head -n $i | tail -n 1 > $workdir/markers/$i
-done
-echo $max > $workdir/max
-echo 0 > $workdir/progress
+create_workdir() {
+  if [ -f "$target" ] && [ -z "$markerfile" ]; then
+    markerfile=${target}.mark
+  elif [ ! -f $target ] && [ -z "$markerfile" ]; then
+    is_target_on_path=$(which $target 2>/dev/null)
+    if [ -n "$is_target_on_path" ]; then
+      echo "Target '$target' is on path but marker file not specified"
+      exit 1
+    fi
+  fi
+  if [ -z "$workdir" ]; then
+    filename=$(basename $target)
+    workdir=/tmp/cit/monitor/$filename
+  fi 
+  mkdir -p $workdir/markers
+  echo "PENDING" > $workdir/status
+}
 
-# if the current directory is not on the path and we just 
-# run "xyz" file in current directory, the shell will not find it.
-# but if we run "./xyz" the shell will find it.  so using
-# dirname and basename here to turn "xyz" into "./xyz" while
-# keeping "/root/xyz" as "/root/xyz" in case an absolute path was
-# provided.
-if [ -f "$target" ]; then chmod +x $target; fi
-#target_dirname=$(dirname $target)
-#target_basename=$(basename $target)
-$target 1>$workdir/stdout 2>$workdir/stderr  &
-target_pid=$!
-echo "ACTIVE" > $workdir/status
+init_markers() {
+  # split the marker file into individual numbered lines in separate files
+  # the grep removes empty lines
+  max=$(cat $markerfile | grep -v -e '^$' | wc -l)
+  for i in `seq 1 $max`
+  do
+    cat $markerfile | grep -v -e '^$' | head -n $i | tail -n 1 > $workdir/markers/$i
+  done
+  echo $max > $workdir/max
+  echo 0 > $workdir/progress
+}
 
-# we're looking for the following things:
-# 1. check that the target is still running
-# 2. identify the next marker in the output
-progress=0
-next=1
-percent=0
-bricks=0
-spaces=0
-bar_width=50
+execute_target() {
+  # if the current directory is not on the path and we just 
+  # run "xyz" file in current directory, the shell will not find it.
+  # but if we run "./xyz" the shell will find it.  so using
+  # dirname and basename here to turn "xyz" into "./xyz" while
+  # keeping "/root/xyz" as "/root/xyz" in case an absolute path was
+  # provided.
+  if [ -f "$target" ]; then chmod +x $target; fi
+  #target_dirname=$(dirname $target)
+  #target_basename=$(basename $target)
+  $target 1>$workdir/stdout 2>$workdir/stderr  &
+  target_pid=$!
+  echo "ACTIVE" > $workdir/status
+  echo $target_pid > $workdir/pid
+}
+
+is_target_running() {
+    local target_pid="$1"
+    if [ -n "$target_pid" ]; then
+        local pid_status=$(ps --pid $target_pid -o pid= -o comm= | awk '{print $1}')
+        if [ -n "$pid_status" ]; then return 0; fi
+    fi
+    return 1
+}
+
+# postcondition:
+#   status is ACTIVE and return code 0, or other status and return code 1
+# 
+is_target_active_running() {
+  if [ -d "$workdir" ]; then
+    status=$(cat $workdir/status 2>/dev/null)
+    max=$(cat $workdir/max 2>/dev/null)
+    target_pid=$(cat $workdir/pid 2>/dev/null)
+    if [ "$status" == "ACTIVE" ] && is_target_running $target_pid; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
+is_target_active_resumable() {
+  if [ -d "$workdir" ]; then
+    status=$(cat $workdir/status 2>/dev/null)
+    max=$(cat $workdir/max 2>/dev/null)
+    target_pid=$(cat $workdir/pid 2>/dev/null)
+    if [ "$status" == "ACTIVE" ] && ! is_target_running $target_pid; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
+
+#  if [ ! -d "$workdir" ]; then
+#    echo "Directory does not exist: $workdir"
+#    return 1
+#  fi
+#  if [ "$status" == "DONE" ]; then
+#    echo "$target is already done"
+#    return 1
+#  elif [ "$status" == "ERROR" ]; then
+#    echo "$target stopped because of an error"
+#    return 1
+#  elif [ "$status" == "PENDING" ]; then
+#    echo "$target has not yet started"
+#    return 1
+#  elif [ "$status" == "ACTIVE" ]; then
+#    # ensure the pid is running
+#    if is_target_running $target_pid; then
+#      return 0
+#    else
+#      echo "$target is not running"
+#      return 1
+#    fi
+#  else
+#    echo "$target status is unknown"
+#    return 1
+#  fi
+
+
 
 # requires global vars 'percent' and 'bar_width' to be set
 progress_bar() {
@@ -107,14 +233,6 @@ progress_bar() {
     echo -n " "
   done
   echo -ne "$percent%\r"
-}
-
-target_running() {
-  is_running=$(ps | awk '{print $1}' | grep "^$target_pid$" | grep -v grep)
-  if [ -n "$is_running" ]; then
-    return 0
-  fi
-  return 1
 }
 
 # preconditions:
@@ -144,7 +262,21 @@ update_progress() {
   done
 }
 
-while [ $progress -lt $max ] && target_running; do
+
+monitor_target() {
+
+# we're looking for the following things:
+# 1. check that the target is still running
+# 2. identify the next marker in the output
+progress=0
+next=1
+percent=0
+bricks=0
+spaces=0
+bar_width=50
+
+
+while [ $progress -lt $max ] && is_target_running; do
   # echo progress without newline and with escape symbols,
   # the backslash r puts the cursor at the beginning of the line
   progress_bar
@@ -160,17 +292,34 @@ progress_bar
 # remove the temporary marker files
 rm -rf $workdir/markers
 
-# either target finished successfully or exited early
-# check the exit code
-wait $target_pid
-target_exit_code=$?
+if [ -n "$MONITOR_NOEXEC" ]; then
+  # wait until the target exits and writes the code
+  while [ -z "$target_exit_code" ]; do
+    if [ -f $workdir/exit ]; then
+      target_exit_code=$(cat $workdir/exit)
+    else
+      sleep 1
+    fi
+  done
+else
+  # normal operation
+  # either target finished successfully or exited early
+  # check the exit code
+  wait $target_pid
+  target_exit_code=$?
+  echo $target_exit_code > $workdir/exit
+  if [ $target_exit_code -eq 0 ]; then
+    echo "DONE" > $workdir/status
+  else
+    echo "ERROR" > $workdir/status
+  fi
+fi
+
 if [ $target_exit_code -eq 0 ]; then
-  echo "DONE" > $workdir/status
   echo -en "$TERM_COLOR_GREEN"
   progress_bar
   echo -en "$TERM_COLOR_NORMAL"
 else
-  echo "ERROR" > $workdir/status
   echo -en "$TERM_COLOR_RED"
   progress_bar
   echo -en "$TERM_COLOR_NORMAL"
@@ -180,4 +329,5 @@ fi
 echo
 
 # exit with target's exit code
-exit $target_exit_code
+return $target_exit_code
+}
